77136d525bff75c7bb14a41476ddb320e6f7810d
rodolfoalvarenga <rodolfo.l.alvarenga@gmail.com>
2024-08-09 11:44:06 -0300

    video07: added servlet context-path

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping(value = {"hi", "hi/"})
+    @GetMapping("hi")
     public String hi() {
         return "OMAE WA MOU SHINDE IRU";
     }
