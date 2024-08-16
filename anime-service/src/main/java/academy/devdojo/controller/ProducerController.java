7b944dfaff37c7bd25cc467bad1dc5901bd358f5
rodolfoalvarenga <rodolfo.l.alvarenga@gmail.com>
2024-08-16 15:10:59 -0300

    video17: ResponseEntity

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -3,10 +3,11 @@ package academy.devdojo.controller;
 import academy.devdojo.domain.Producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -34,11 +35,14 @@ public class ProducerController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-key")
-    public Producer save(@RequestBody Producer producer, @RequestHeader HttpHeaders headers) {
+    public ResponseEntity<Producer> save(@RequestBody Producer producer, @RequestHeader HttpHeaders headers) {
         log.info("{}", headers);
         producer.setId(ThreadLocalRandom.current().nextLong(100_000));
         Producer.getProducers().add(producer);
-        return producer;
+        var responseHeaders = new HttpHeaders();
+        responseHeaders.add("Authorization", "My Key");
+
+        return ResponseEntity.status(HttpStatus.CREATED).headers(responseHeaders).body(producer);
     }
 
 }
